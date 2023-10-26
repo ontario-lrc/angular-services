7a351223764a4a4db03a305efccff75740299dc3
Kasperowicz, Dawid <dawid.kasperowicz@ontario.ca>
2023-10-26 16:56:29 -0400

    name change for services

--- projects/ods-accordion-group-service/src/lib/accordion-group.service.ts
+++ projects/ods-accordion-group-service/src/lib/accordion-group.service.ts
@@ -5,7 +5,7 @@ import {Injectable} from "@angular/core";
 	providedIn: "root"
 })
 
-export class AccordionGroupService
+export class OdsAccordionGroupService
 {
 	private _accordionsInGroup: HTMLButtonElement[] = [];
 
